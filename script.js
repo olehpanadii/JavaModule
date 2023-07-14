067889f7b47a575b73d28d6f0d1cea4dd38d8333
OlegPanadiy <opanadiy@gmail.com>
2023-07-14 12:05:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -75,3 +75,19 @@
 //   // Change code above this line
 //   return message;
 // }
+
+// const atTheOldToad = {
+//   // Change code below this line
+//   potions: [],
+
+//   // Change code above this line
+// };
+// console.log(atTheOldToad);
+
+// Додай об'єкту atTheOldToad метод getPotions(), який просто повертає значення властивості potions.
+
+// Оголошена змінна atTheOldToad
+// Значення змінної atTheOldToad - це об'єкт
+// Значення властивості atTheOldToad.potions - це масив ["Speed potion", "Dragon breath", "Stone skin"]
+// Значення властивості atTheOldToad.getPotions - це метод об'єкта
+// Виклик методу atTheOldToad.getPotions() повертає ["Speed potion", "Dragon breath", "Stone skin"]
